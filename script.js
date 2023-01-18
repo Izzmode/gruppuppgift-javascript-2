e0652d2832bd0288222a2664047d27c6b6d59a73
c4trisk <cathja.riska@gmail.com>
2023-01-18 11:52:49 +0100

    comments

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 const BASE_URL = ' https://fnd22-shared.azurewebsites.net/api/cases/';
 const COMMENTS_URL = ' https://fnd22-shared.azurewebsites.net/api/comments/';
 const STATUSES_URL = ' https://fnd22-shared.azurewebsites.net/api/statuses/';
+
+
+
+
+// HÄMTA CASES FRÅN API
+
+// LIST CASES - Lista upp dem i rätt ordning
+
+// CREATE ELEMENT - Skapa html-kort av cases
+
+// VALIDATE ADD-FORM - Validera lägg till ärende-formuläret
+
+// ADD CASE TO DATABASE - Lägger till ett nytt ärende till apit
+
+
+// TILL DETAILS.JS
+
+// FETCH CASE BY ID - URL Query
+
+// CREATE ELEMENT - skapar html-element med all info från caset
+
+// VALIDATE COMMMENT-FORM
+
+// UPDATE CASE IN DATABASE - Uppdaterar caset med kommentar
+
+// UPDATE STATUS IN DATABASE - Ändrar 0,1,2 i statusen, skickar till databasen
+
+
+// EVENT LISTENER
+
+// ADD-FORM SUBMIT (validatefunktionen körs + create Element + Add to Database)
+
+// OPEN CASE (tar en till details-sidan som visar specifics)
+
+
+// DETAILS.JS
+
+// COMMENT-FORM SUBMIT (validate-funktion + crate Element + uppdatera databasen)
+
+// STATUS CHANGE-FORM (uppdate status-funktion, hämtar infon på nytt från databasen (fetch / create element))
