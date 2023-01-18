1f3de06f47b5f7b75667e3fa18ad583717a7bc7c
Izabelle Olofsson <izabelleolofsson@live.se>
2023-01-18 14:36:52 +0100

    lagt till partial fil samt ändrat i html

--- details.js
+++ details.js
@@ -4,7 +4,8 @@ const STATUSES_URL = ' https://fnd22-shared.azurewebsites.net/api/statuses/';
 
 const id = new URLSearchParams(window.location.search).get('id');
 
-const output = document.querySelector('.output')
+const output = document.querySelector('.output');
+const cardWrapper = document.querySelector('.card-wrapper');
 
 // console.log(id);
 
@@ -25,25 +26,29 @@ const createElement = (data) => {
 
     const statusText = document.createElement('p');
     statusText.classList.add('status');
-    statusText.innerText = data.subject;
+    statusText.innerText = data[3].status.statusName;
 
-    const h1 = document.createElement('h1')
-    h1.classList.add('subject')
+    const h1 = document.createElement('h1');
+    h1.classList.add('subject');
+    h1.innerText = data[3].subject;
 
     const descriptionText = document.createElement('p');
     descriptionText.classList.add('descriptionText');
+    descriptionText.innerText = data[3].message;
 
     const userEmail = document.createElement('p');
     userEmail.classList.add('userEmail');
+    userEmail.innerText = data[3].email;
 
     const timeStamp = document.createElement('p');
     timeStamp.classList.add('timeStamp');
+    timeStamp.innerText = data[3].created;
 
     div.appendChild(statusText)
     div.appendChild(h1)
     div.appendChild(descriptionText)
     div.appendChild(userEmail)
     div.appendChild(timeStamp)
-    output.appendChild(div);
+    cardWrapper.appendChild(div);
 
 }
