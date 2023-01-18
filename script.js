d105d4a7940349fc0926c67ac3ca05b3e1551268
Izabelle Olofsson <izabelleolofsson@live.se>
2023-01-18 11:23:28 +0100

    lade till base url

---
+++ script.js
@@ -0,0 +1,3 @@
+const BASE_URL = ' https://fnd22-shared.azurewebsites.net/api/cases/';
+const COMMENTS_URL = ' https://fnd22-shared.azurewebsites.net/api/comments/';
+const STATUSES_URL = ' https://fnd22-shared.azurewebsites.net/api/statuses/';
